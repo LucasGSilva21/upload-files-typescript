e6a22ded977875eeff3b21c0fb4714691797376b
Lucas Gabriel Silva <lucasgsilva2102@gmail.com>
2022-03-13 19:17:28 -0300

    refactor: change ts to js

--- backend/scripts/generate-file.js
+++ backend/scripts/generate-file.js
@@ -1,12 +1,12 @@
-import fs from 'fs'
-import { randomBytes } from 'crypto'
+const fs = require('fs')
+const { randomBytes } = require('crypto')
 
 const writeStream = fs.createWriteStream('big.file')
 
 writeStream.write(randomBytes(1e9), 'base64')
 
 writeStream.on('finish', () => {
-    console.log('wrote all data to file')
+    console.log('finish')
 })
 
 writeStream.end()
