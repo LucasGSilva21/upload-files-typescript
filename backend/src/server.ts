bdf7aae2ca0ebe181e09e21c6a6259db4557e7bd
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2022-03-15 17:13:37 -0300

    refactor: move express setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,6 @@
-import express from 'express'
-import { routes } from './routes'
+import { app } from './app'
 
 const PORT = 3333
-const app = express()
-
-app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`Running at http://localhost:${PORT}`)
