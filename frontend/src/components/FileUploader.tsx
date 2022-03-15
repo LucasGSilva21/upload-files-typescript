49cdef4d36c0c93c83f85ffcee6dbf0c1df75386
Lucas Gabriel Silva <lucasgsilva2102@gmail.com>
2022-03-14 22:59:10 -0300

    feat: improve layout

--- frontend/src/components/FileUploader.tsx
+++ frontend/src/components/FileUploader.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
+import { makeStyles } from '@material-ui/core/styles'
+import { Grid, Input, Button } from '@material-ui/core'
+import { CloudUpload, Clear } from '@material-ui/icons'
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}))
 
 export const FileUpload = () => {
-  const [selectedFile, setSelectedFile] = React.useState(null)
+  const classes = useStyles()
+
+  const [selectedFile, setSelectedFile] = useState<any>(null)
+  const [isSelected , setIsSelected] = useState(false)
 
   const handleSubmit = async (event: any) => {
     event.preventDefault()
@@ -22,13 +34,56 @@ export const FileUpload = () => {
 
   const handleFileSelect = (event: any) => {
     setSelectedFile(event.target.files[0])
+    setIsSelected(true)
+  }
+
+  const cleanFileSelect = () => {
+    setIsSelected(false)
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="file" onChange={handleFileSelect}/>
-      <input type="submit" value="Upload File" />
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Input type="file" onChange={handleFileSelect}/>
+        {isSelected ? (
+          <div>
+            <p>Nome do arquivo: {selectedFile?.name}</p>
+            <p>Tipo do arquivo: {selectedFile?.type ?? 'Não identificado'}</p>
+            <p>Tamanho em bytes: {selectedFile?.size}</p>
+            <p>
+              Última modificação:{' '}
+              {selectedFile?.lastModifiedDate.toLocaleDateString()}
+            </p>
+          </div>
+        ) : (
+          <p>Selecione um arquivo para ver os detalhes</p>
+        )}
+        <br/>
+        <Button
+          type="reset"
+          variant="contained"
+          color="default"
+          className={classes.button}
+          startIcon={<Clear />}
+          onClick={cleanFileSelect}
+        >
+          Limpar
+        </Button>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          startIcon={<CloudUpload />}
+        >
+          Enviar
+        </Button>
+      </Grid>
     </form>
   )
 }
-
--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import { FileUpload } from '../components/FileUploader'
+import { Grid } from '@material-ui/core'
 
 export const App = () => {
-  return <FileUpload/>
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <h1>Upload de arquivos</h1>
+      <FileUpload/>
+    </Grid>
+  )
 }
