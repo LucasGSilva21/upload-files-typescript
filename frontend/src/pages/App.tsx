4b4ba05b23cea472526c3af303cb098cef8e1518
Lucas Gabriel Silva <lucasgsilva2102@gmail.com>
2022-03-21 08:59:02 -0300

    feat: improve upload

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import { FileUpload } from '../components/FileUploader'
 import { Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  upload: {
+    height: '100vh',
+  },
+  border: {
+    border: '5px solid black',
+    borderRadius: '25px',
+    padding: '25px'
+  },
+}))
 
 export const App = () => {
+  const classes = useStyles()
+
   return (
     <Grid
       container
       direction="column"
       justifyContent="center"
       alignItems="center"
+      className={classes.upload}
     >
-      <h1>Upload de arquivos</h1>
-      <FileUpload/>
+      <div className={classes.border}>
+        <h1>Upload de arquivos</h1>
+        <FileUpload/>
+      </div>
     </Grid>
   )
 }
